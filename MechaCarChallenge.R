cf2d19d1e7c61854670b468f9cbb5ae85b704c24
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-06-04 22:21:22 -0500

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ mCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mCar)
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mCar)) 
+#import csv file as a table
+sus_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#create summary table with the three columns
+total_summary <- sus_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD= sd(PSI), .groups = 'keep') 
+
+lot_summary <- sus_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD= sd(PSI), .groups = 'keep') 
