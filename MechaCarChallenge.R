9afb6f11f45d997acd1ed6b52b8bafce813c22c3
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-06-05 22:30:21 -0500

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,15 @@ sus_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors 
 total_summary <- sus_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD= sd(PSI), .groups = 'keep') 
 
 lot_summary <- sus_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD= sd(PSI), .groups = 'keep') 
+
+#compare PSI versus population means
+t.test((sus_table$PSI),mu=1500)
+
+#sus_Lot1 <- sus_table %>% filter(Manufacturing_Lot=="Lot1") 
+#sus_Lot2 <- sus_table %>% filter(Manufacturing_Lot=="Lot2")
+#sus_Lot3 <- sus_table %>% filter(Manufacturing_Lot=="Lot3")#select only data points where the year is 1999
+
+#t.test((sus_Lot1$PSI), (sus_Lot2$PSI),  mu=1500)
+t.test((sus_table$PSI),mu=1500, subset=(sus_table$Manufacturing_Lot=="Lot1"))
+t.test((sus_table$PSI),mu=1500, subset=(sus_table$Manufacturing_Lot=="Lot2"))
+t.test((sus_table$PSI),mu=1500, subset=(sus_table$Manufacturing_Lot=="Lot3"))
